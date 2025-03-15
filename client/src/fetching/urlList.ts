dad41fa6a24647c386f14a638f35103c067fd5f4
s3nsh1-dev <shubham228pandey@gmail.com>
2025-03-15 23:05:43 +0530

    i need to do better

--- client/src/fetching/urlList.ts
+++ client/src/fetching/urlList.ts
@@ -1,5 +1,8 @@
-export const URL_1: string =
+export const openMetroWeatherAPI: string =
   "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_180m&current=is_day";
 
 export const placeholderURL: string =
   "https://jsonplaceholder.typicode.com/posts";
+
+export const OpenStreetMapAPI: string =
+  "https://nominatim.openstreetmap.org/search?q=New+York&format=json";
