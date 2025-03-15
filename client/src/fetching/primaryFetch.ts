a2925a11f795797ff7357c64232ed1b01b3ffe3d
s3nsh1-dev <shubham228pandey@gmail.com>
2025-03-15 09:47:47 +0530

    ts run in es without async

--- client/src/fetching/primaryFetch.ts
+++ client/src/fetching/primaryFetch.ts
@@ -2,15 +2,15 @@ import { placeholderURL } from "./urlList";
 
 let finalResult: any = null;
 
-try {
-  const fetchingURL = async (): Promise<Response> => {
-    const response = await fetch(placeholderURL);
-    if (!response.ok) {
-      throw new Error("HTTP error, status = " + response.status);
-    }
-    return await response.json();
-  };
+export default async function fetchingURL(): Promise<Response> {
+  const response = await fetch(placeholderURL);
+  if (!response.ok) {
+    throw new Error("HTTP error, status = " + response.status);
+  }
+  return await response.json();
+}
 
+try {
   finalResult = await fetchingURL();
 } catch (error: unknown) {
   console.error("Error fetching URL", error);
