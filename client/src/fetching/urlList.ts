bae5715e66aae4562e577f8b0c46627b5379013e
s3nsh1-dev <shubham228pandey@gmail.com>
2025-03-15 05:49:07 +0530

    added urlList

--- client/src/fetching/urlList.ts
+++ client/src/fetching/urlList.ts
@@ -1,4 +1,4 @@
-export const URL: string =
+export const URL_1: string =
   "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_180m&current=is_day";
 
 export const placeholderURL: string =
