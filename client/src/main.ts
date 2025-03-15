63336fe235b5aea7eb411d555d1252618abc68b4
s3nsh1-dev <shubham228pandey@gmail.com>
2025-03-15 10:15:04 +0530

    weather package installed

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,3 +1,5 @@
 import { finalResult as result } from "./fetching/primaryFetch";
+import { weatherData } from "./fetching/websiteCode";
 
 console.log("data", result);
+console.log("weather", weatherData);
