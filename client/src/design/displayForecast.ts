2530bd5832affd1ed71de3afba235714e1e00fe7
s3nsh1-dev <shubham228pandey@gmail.com>
2025-03-17 00:20:16 +0530

    base UI partitioned

---
+++ client/src/design/displayForecast.ts
@@ -0,0 +1,4 @@
+export default function displayForecast() {
+  const displayElem = document.querySelector<HTMLDivElement>("#choice-result")!;
+  //   displayElem.innerHTML = `i am a disco dancer`;
+}
