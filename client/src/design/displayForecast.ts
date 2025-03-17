408e77cb9bd324932e16b05374b86a088f88c113
s3nsh1-dev <shubham228pandey@gmail.com>
2025-03-17 13:11:59 +0530

    switch&reset UI fixed

--- client/src/design/displayForecast.ts
+++ client/src/design/displayForecast.ts
@@ -1,15 +1,16 @@
 import { changeButtonValues } from "../main";
+import "./design-style/displayForecast.css";
 
 export default function displayForecast() {
   const displayElem = document.querySelector<HTMLDivElement>("#choice-result")!;
   displayElem.innerHTML = `
         <nav id="switch-panel">
           <div class="divButtons" id="switch-to-weather">Weather</div>
-          <div class="divButtons" id="switch-to-forecast">Forecast</div>
+          <div class="divButtons selected-divButton" id="switch-to-forecast">Forecast</div>
         </nav>
         <article id="weather-forecast">
-        <div>
-            <button id="reset">---back---</button>
+        <div class="reset-container">
+            <button id="reset">Go Back</button>
             <h1 id="panel-heading">Weather</h1>
         </div>
           <div id="card-container">
@@ -18,11 +19,30 @@ export default function displayForecast() {
         </article>`;
   console.log("displayForecast");
   handleResetClick();
+  handlePanelSwitching();
 }
 
 const handleResetClick = () => {
   const btnElement = document.querySelector<HTMLButtonElement>("#reset")!;
   btnElement.addEventListener("click", () => {
     changeButtonValues();
+  });
+};
+
+const handlePanelSwitching = () => {
+  const weather = document.querySelector<HTMLDivElement>("#switch-to-weather")!;
+  const forecast = document.querySelector<HTMLDivElement>(
+    "#switch-to-forecast"
+  )!;
+  const panelHeading = document.getElementById("panel-heading")!;
+  weather.addEventListener("click", () => {
+    weather.classList.remove("selected-divButton");
+    forecast.classList.add("selected-divButton");
+    panelHeading.innerText = "Weather";
+  });
+  forecast.addEventListener("click", () => {
+    forecast.classList.remove("selected-divButton");
+    weather.classList.add("selected-divButton");
+    panelHeading.innerText = "Forecast";
   });
 };
