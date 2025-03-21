e7825f2f50d5c5b1f6d75392b1e1542ebc2a48a8
s3nsh1-dev <shubham228pandey@gmail.com>
2025-03-21 08:58:25 +0530

    patched up code

--- client/src/design/format-data/cardContainer.ts
+++ client/src/design/format-data/cardContainer.ts
@@ -8,38 +8,13 @@ import morning from "../../../public/images/morning.jpg";
 import evening from "../../../public/images/evening.png";
 import dawn from "../../../public/images/dawn.jpg";
 
-const timeArray = [
-  {
-    time: "",
-    period: "Midnight",
-    imageSrc: midnight,
-    date: "",
-    temperature: "",
-  },
-  { time: "", period: "Dawn", imageSrc: "image2", date: "", temperature: "" },
-  {
-    time: "",
-    period: "Morning",
-    imageSrc: morning,
-    date: "",
-    temperature: "",
-  },
-  {
-    time: "",
-    period: "Afternoon",
-    imageSrc: afternoon,
-    date: "",
-    temperature: "",
-  },
-  {
-    time: "",
-    period: "Evening",
-    imageSrc: evening,
-    date: "",
-    temperature: "",
-  },
-  { time: "", period: "Night", imageSrc: night, date: "", temperature: "" },
-];
+interface timeType {
+  time: string;
+  period: string;
+  imageSrc: any;
+  date: string;
+  temperature: string;
+}
 
 export default async function cardContainer() {
   const containerElement = document.getElementById(
@@ -49,21 +24,58 @@ export default async function cardContainer() {
     console.error("card-container element not found");
     return;
   }
+
   const coObject = await fetchForCoordinates();
-  //   console.log("Name:", coObject.name);
   const fresh_URL = getWeatherAPI(coObject.lon, coObject.lat, "current");
-  const response = await fetchingURL(fresh_URL);
-  const data = await response.json();
-  console.log("what we get:", data);
-  console.log("NEXT:", data.current.temperature_2m);
-  let cardsHTML = "";
-  for (let i = 0; i < 6; i++) {
-    cardsHTML += `<div class="w-cards">
-    <img src="${image}" alt="weather_pic"/>
-    <div class="displayBox">
-      <div class="WhatTime">${date}: ${time}</div>
-      <div class="whatTemperature">${temperature}</div>
-    </div>`;
-    containerElement.innerHTML = cardsHTML;
-  }
+  const response: Response = await fetchingURL(fresh_URL);
+  const data_API: any = await response.json();
+  const finalTimeObject = prepareTimeObject(data_API);
+  console.log("Time Array : ", finalTimeObject);
+  // let cardsHTML = "";
+  // for (let i = 0; i < 6; i++) {
+  //   cardsHTML += `<div class="w-cards">
+  //   <img src="${image}" alt="weather_pic"/>
+  //   <div class="displayBox">
+  //     <div class="WhatTime">${date}: ${time}</div>
+  //     <div class="whatTemperature">${temperature}</div>
+  //   </div>`;
+  //   containerElement.innerHTML = cardsHTML;
+  // }
+}
+
+function prepareTimeObject(data_API: any) {
+  const timeArray: timeType[] = [
+    {
+      time: "",
+      period: "Midnight",
+      imageSrc: midnight,
+      date: "",
+      temperature: "",
+    },
+    { time: "", period: "Dawn", imageSrc: dawn, date: "", temperature: "" },
+    {
+      time: "",
+      period: "Morning",
+      imageSrc: morning,
+      date: "",
+      temperature: "",
+    },
+    {
+      time: "",
+      period: "Afternoon",
+      imageSrc: afternoon,
+      date: "",
+      temperature: "",
+    },
+    {
+      time: "",
+      period: "Evening",
+      imageSrc: evening,
+      date: "",
+      temperature: "",
+    },
+    { time: "", period: "Night", imageSrc: night, date: "", temperature: "" },
+  ];
+  console.log("Fetched API data", data_API);
+  return timeArray;
 }
